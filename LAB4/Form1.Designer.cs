a1513a640bd49c99ca147c0d04d5da985bd1763b
Bambus <frajer@gmail>
2025-03-27 11:06:10 +0100

    versja 2

--- LAB4/Form1.Designer.cs
+++ LAB4/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             button1 = new Button();
             pictureBox1 = new PictureBox();
+            button2 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            button3 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -39,7 +44,7 @@
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 4;
-            button1.Text = "load";
+            button1.Text = "loudu";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
@@ -51,21 +56,86 @@
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
             // 
+            // button2
+            // 
+            button2.Location = new Point(76, 125);
+            button2.Name = "button2";
+            button2.Size = new Size(151, 23);
+            button2.TabIndex = 6;
+            button2.Text = "rotatu";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(76, 50);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(94, 19);
+            radioButton1.TabIndex = 7;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(76, 75);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 19);
+            radioButton2.TabIndex = 8;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(76, 100);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(94, 19);
+            radioButton3.TabIndex = 9;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(76, 154);
+            button3.Name = "button3";
+            button3.Size = new Size(151, 23);
+            button3.TabIndex = 10;
+            button3.Text = "invertu koloru";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(button2);
             Controls.Add(pictureBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private Button button1;
         private PictureBox pictureBox1;
+        private Button button2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Button button3;
     }
 }
--- LAB4/Form1.cs
+++ LAB4/Form1.cs
@@ -14,13 +14,53 @@ namespace LAB4
 
         private void button1_Click(object sender, EventArgs e)
         {
-          using (OpenFileDialog
-                openFileDialog = new OpenFileDialog()) { openFileDialog.Title = "chus the pikczer";
+            using (OpenFileDialog
+                  openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "chus the pikczer";
                 openFileDialog.Filter = "pliki graficzne|*.jpg;*.jpeg;*.png;*.gif|Wszystkie pliki|*.*";
                 if (openFileDialog.ShowDialog() == DialogResult.OK) { pictureBox1.Image = Image.FromFile(openFileDialog.FileName); }
             }
         }
 
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Refresh();
+            }
+            if (radioButton2.Checked)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                pictureBox1.Refresh();
+            }
+            if (radioButton3.Checked)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color pixelColor = bmp.GetPixel(x, y);
+                    Color invertedColor = Color.FromArgb(255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B);
+                    bmp.SetPixel(x, y, invertedColor);
+                }
+            }
+            pictureBox1.Image = bmp;
+            pictureBox1.Refresh();
+        }
     }
 }
